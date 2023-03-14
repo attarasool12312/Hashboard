7593ee98516f77d56b0f4525a4acff6a7155e159
Hamayun Aziz <hamayunaziz@codonist.com>
2023-03-14 20:52:05 +0500

    new-hashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,26 @@ uploadBotton.onchange = () => {
 
 
 
+// popup 
+
+jQuery(($) => {
+  $('.attachment input[type="file"]')
+    .on('change', (event) => {
+    let el = $(event.target).closest('.attachment').find('.btn-file');
+    
+    el
+      .find('.btn-file__actions__item')
+      .css({
+        'padding': '20px'
+      });
+    
+    el
+      .find('.btn-file__preview')
+      .css({
+        'background-image': 'url(' + window.URL.createObjectURL(event.target.files[0]) + ')'
+      });
+  });
+});
 
 
 

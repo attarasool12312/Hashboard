49a05feac6055a0bcd6530923ced2ee8f38bbce7
Hamayun Aziz <hamayunaziz@codonist.com>
2023-05-25 19:43:00 +0500

    user flow page

--- assets/js/my-journey.js
+++ assets/js/my-journey.js
@@ -50,3 +50,19 @@ function addDepartmentChannels() {
   </div>
 </div>`);
 }
+
+const colorPicker = document.getElementById('colorPicker');
+const selectedColor = document.getElementById('selectedColor');
+
+colorPicker.addEventListener('input', function () {
+  const color = colorPicker.value;
+  selectedColor.style.backgroundColor = color;
+});
+
+const colorPickerBackground = document.getElementById('colorPickerBackground');
+const selectedColorBackground = document.getElementById('selectedColorBackground');
+
+colorPickerBackground.addEventListener('input', function () {
+  const color = colorPickerBackground.value;
+  selectedColorBackground.style.backgroundColor = color;
+});
